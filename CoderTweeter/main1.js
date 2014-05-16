ccd7ca360310790058f498f392b1dd8eea228d7f
asglasby <aishaextra@gmail.com>
2014-05-16 15:17:33 -0400

    Started a getTweetsArray

--- CoderTweeter/main1.js
+++ CoderTweeter/main1.js
@@ -64,8 +64,11 @@ var userName;
 var allUsers = [];
 var allUsersObject = {};
 var myurl = "https://codercamptweeter.firebaseio.com/.json";
-//var keyHolder = [];
 var loggedInUser = "";
+var getTweetsArray = [];
+
+//This array will be used to store new tweets that will be sent to the Firebase server from the array.
+var sendTweetArray = [];
 
 var Kyle = {
     name: "Kyle",
@@ -153,9 +156,6 @@ var NewTweet = function (userName, message, result) {
     this["timeStamp"] = result;
 };
 
-//This array will be used to store new tweets that will be sent to the Firebase server from the array.
-var newTweetArray = [];    
-    
 // This function will pull the tweets from the Firebase server.
 var getTweets = function () {
     "use strict";
@@ -195,9 +195,7 @@ var getTweets = function () {
     request.send();//what ever is put inside send is posted to the server.  Since our tweet is an object, the JSON stringify will turn it into a string.  If whatever we are sending is already a string, we do not need to JSON.stringify it.
 };
 
-
 //to retweet, I could make another sendTweet call. I need to pass it the object and add it an original author property
-
 var reTweet = function (tweetToRetweet) {
 
     tweetToRetweet["userName"] = loggedInUser;
@@ -263,37 +261,10 @@ var sendTweet = function () {
     }
     request.send(JSON.stringify(tweet));//what ever is put inside send is posted to the server.  Since our tweet is an object, the JSON stringify will turn it into a string.  If whatever we are sending is already a string, we do not need to JSON.stringify it.
 
-    newTweetArray.push(tweet);
+    sendTweetArray.push(tweet);
     //alert(JSON.stringify(newTweetArray));
     //sendTweetPost();
 };
-
-//var sendTweetPost = function () {
-//    "use strict";
-    
-//    var request = new XMLHttpRequest();
-//    request.open("POST", myurl, true); // Post will send the information to firebase
-
-//    // the onload is what we want to happen when the request comes base from firebase.
-//    request.onload = function (event) {
-//        if (this.status >= 200 && this.status < 400) {
-//            //this is what happens when our request is successful later.
-//            var data = JSON.parse(this.response); //this will parse my response which will be a key that will be retuned as an object, which can be used.  The key is letting me know how to access my key later.
-//            console.log(data);
-//        } else {
-//            //this is was happens when the request fails
-//            console.log(this.response);
-//        }
-//        getTweets();
-//    };
-//    // This lets us know what to do when an error occured.  Either you or the server is offline.
-//    request.onerror = function () {
-//        //This on error is for when the connection fails
-//        console.log("Whoops, connection failed!");
-//    }
-//    request.send(JSON.stringify(newTweetArray));//what ever is put inside send is posted to the server.  Since our tweet is an object, the JSON stringify will turn it into a string.  If whatever we are sending is already a string, we do not need to JSON.stringify it.
-
-//};
    
 var follow = function () {
     "use strict";
