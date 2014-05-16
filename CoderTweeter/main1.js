1638f039bff267a6a56cf121e54d5771d78e735c
dunnk2 <kjdunn02@gmail.com>
2014-05-15 20:12:17 -0500

    Fixed retweet bug

--- CoderTweeter/main1.js
+++ CoderTweeter/main1.js
@@ -24,53 +24,59 @@
         2. tweet method - works
         3. Login method - works
         4. sign up method - works
-        4. retweet
+        4. retweet - works
         5. Somewhere, use a prototype
-     
-     still to do- follow, retweet, 
+
+      Tasks 
+        -add a closure on our tweet constructor so users can't change tweet content
+        -add a prototype for user constructor so each user has a default profile pic
+        -add css
+        -
+ 
 
 */
-//var tweet = document.getElementById().value + 
 
-//An array meant to hold all the usernames for our app. Right now its holding strings, I need to change that to objects. 
-/*
-var allUsers = [];
-
-var Kyle = {
-    name: "Kyle"
-};
-var Aisha = {
-    name: "Aisha""
-};
-var Christi = {
-    name: "Christi"
-};
-
-allUsers.push(Kyle, Aisha, Christi);
-*/
 
 var userName;
 var allUsers = [];
+allUsersObject = {};
 myurl = "https://codercamptweeter.firebaseio.com/.json";
 //var keyHolder = [];
 var loggedInUser = "";
 
 var Kyle = {
-    name: "Kyle"
+    name: "Kyle",
+    image: "images/meerkat.jpg"
 };
 var Aisha = {
-    name: "Aisha"
+    name: "Aisha",
+    image: "images/meerkat.jpg"
 };
 var Christi = {
-    name: "Christi"
+    name: "Christi",
+    image: "images/meerkat.jpg"
 };
 
 allUsers.push(Kyle, Aisha, Christi)
 
+allUsersObject["Kyle"] = Kyle;
+allUsersObject["Aisha"] = Aisha;
+allUsersObject["Christi"] = Christi;
+
 //create a user constructor that will create users as objects
+
+
 var CreateUser = function (name) {
     this.name = name;
+    allUsersObject[name] = name;
 };
+
+var userPrototype = {
+    image: 'images/meerkat.jpg'
+};
+
+
+CreateUser.prototype = userPrototype;
 
 //create a signup function that calls the createUser constructor and pushes it to the array
 var signUp = function () {
@@ -110,9 +116,8 @@ var showHomePage = function () {
         if (found === false) {
             document.getElementById("contentWrapper").innerHTML += "<p class='startPage'>That account could not be found. Sign up if you have not made an account!</p>";
         }
-        
 };
-12
+
 //This is the New Tweet Constructor for the tweets.
 var NewTweet = function (userName, message) {
     this["userName"] = userName;
@@ -137,8 +142,8 @@ var getTweets = function () {
             for (var propName in data) {
                 if (data[propName]["userName"] === userName) {
                 //We are being given an object of objects. 
-                    document.getElementById("container").innerHTML += "<div class='tweet'> " + 
-                    data[propName]["userName"] + ':' + data[propName]["message"] + "<button onclick='reTweet(" + JSON.stringify(data[propName]) + ");'>Retweet</button>" + "<br /></div>";
+                    document.getElementById("container").innerHTML += "<div class='tweet'><img class='images' src=" + allUsersObject[userName]["image"] + " /> " +
+                    data[propName]["originalAuthor"] + ':' + data[propName]["message"] + "<button onclick='reTweet(" + JSON.stringify(data[propName]) + ");'>Retweet</button>" + "<br /></div>";
                     //keyHolder.push(propName["userName"], propName["message"]);
                 }
             }
@@ -156,33 +161,24 @@ var getTweets = function () {
 };
 
 
-//to retweet, I could make another sendTweet call. I need to pass it the object and add it an original author property
-
 var reTweet = function (tweetToRetweet) {
-
     tweetToRetweet["userName"] = loggedInUser;
 
     var request = new XMLHttpRequest();
-    request.open("POST", myurl, true); // Post will send the information to firebase
-
-    //the onload is what we want to happen when the request comes base from firebase.
+    request.open("POST", myurl, true); 
     request.onload = function (event) {
         if (this.status >= 200 && this.status < 400) {
-            //this is what happens when our request is successful later.
-            var data = JSON.parse(this.response); //this will parse my response which will be a key that will be retuned as an object, which can be used.  The key is letting me know how to access my key later.
+            var data = JSON.parse(this.response); 
             console.log(data);
         } else {
-            //this is was happens when the request fails
             console.log(this.response);
         }
         getTweets();
     };
-    // This lets us know what to do when an error occured.  Either you or the server is offline.
     request.onerror = function () {
-        //This on error is for when the connection fails
         console.log("Whoops, connection failed!");
     }
-    request.send(JSON.stringify(tweetToRetweet));//what ever is put inside send is posted to the server.  Since our tweet is an object, the JSON stringify will turn it into a string.  If whatever we are sending is already a string, we do not need to JSON.stringify it.
+    request.send(JSON.stringify(tweetToRetweet));
 };
 
 var sendTweet = function () {
@@ -274,7 +270,7 @@ var getCombinedTweets = function () {
 
                     //We are being given an object of objects. If we turn it into an array of objects, we can loop through the array and sort it by time. To sort by time, 
                     document.getElementById("container").innerHTML += "<div class='tweet'> " + 
-                    data[propName]["userName"] + ':' + data[propName]["message"] + "<img class='retweet' src='images/retweet.jpg'/>" + "<br /></div>";
+                    data[propName]["originalAuthor"] + ':' + data[propName]["message"] + "<img class='retweet' src='images/retweet.jpg'/>" + "<br /></div>";
                     //keyHolder.push(propName["userName"], propName["message"]);
                 }
                 //to retweet, set a counter that gives an id to the div. Get the 
@@ -285,7 +281,7 @@ var getCombinedTweets = function () {
 
                     //We are being given an object of objects. If we turn it into an array of objects, we can loop through the array and sort it by time. To sort by time, 
                     document.getElementById("container").innerHTML += "<div class='tweet'> " +
-                    data[propName]["userName"] + ':' + data[propName]["message"] + "<img class='retweet' src='images/retweet.jpg'/>" + "<br /></div>";
+                    data[propName]["originalAuthor"] + ':' + data[propName]["message"] + "<img class='retweet' src='images/retweet.jpg'/>" + "<br /></div>";
                     //keyHolder.push(propName["userName"], propName["message"]);
                 }
             }
